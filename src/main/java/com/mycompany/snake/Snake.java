3e238d2ab23c968ec96c3ec667d4666746e67965
alu10211999 <alu10211999@ti3-09>
2023-03-30 16:50:36 +0200

    Third day

--- src/main/java/com/mycompany/snake/Snake.java
+++ src/main/java/com/mycompany/snake/Snake.java
@@ -5,6 +5,10 @@
 package com.mycompany.snake;
 
 
+import static com.mycompany.snake.Direction.DOWN;
+import static com.mycompany.snake.Direction.LEFT;
+import static com.mycompany.snake.Direction.RIGHT;
+import static com.mycompany.snake.Direction.UP;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,9 +24,13 @@ public class Snake {
         direction = Direction.RIGHT;
         snake = new ArrayList<Node>();
         
-        for (int i = 0; i < 4; i++) {
-            snake.add(new Node(Config.instance.numRow/2, (Config.instance.numCol-i)/2));
+        for (int i = 0; i < 3; i++) {
+            snake.add(new Node(Config.instance.numRow/2, Config.instance.numCol/2 - i));
         }
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
     }
     
     public boolean containsNode(int row, int col) {
@@ -46,9 +54,52 @@ public class Snake {
               b.drawSquare(g,snake.get(i), Type.BODY); 
             }
             
+            
         }
     }
- 
+    public boolean canMove(int row, int col) { 
+        if (row >= Config.instance.numRow || row < 0 ||
+                col >= Config.instance.numCol || col < 0 || containsNode(row, col) ) {
+            
+            return false;
+        }
+        return true;
+    }
+    
+    public void move() {
+        
+        int row = snake.get(0).getRow();
+        int col = snake.get(0).getCol();
+        
+        
+        switch(direction) {
+            case UP:
+                if (canMove(row - 1, col)) {
+                    snake.add(0, new Node(row - 1 , col));
+                    snake.remove(snake.size()-1);
+                }
+                break;
+            case DOWN:
+                if (canMove(row + 1, col)) {
+                    snake.add(0, new Node(row + 1 , col));
+                    snake.remove(snake.size()-1);
+                }
+                break;
+            case LEFT:
+                if (canMove(row, col - 1)) {
+                    snake.add(0, new Node(row , col - 1));
+                    snake.remove(snake.size()-1);
+                }
+                break;
+            case RIGHT:
+                if (canMove(row, col + 1)) {
+                    snake.add(0, new Node(row , col + 1));
+                    snake.remove(snake.size()-1);
+                    
+                }
+                break;
+        }
+    }
 }
     
     
