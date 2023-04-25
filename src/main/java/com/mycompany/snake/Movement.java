0ea833873367c55de5221c9a1e2c4c495860487c
alu10211999 <alu10211999@ti3-09>
2023-04-25 19:25:26 +0200

    Clase Movement

--- src/main/java/com/mycompany/snake/Movement.java
+++ src/main/java/com/mycompany/snake/Movement.java
@@ -18,52 +18,19 @@ public class Movement {
     
     private List<Direction> movements;
     
-    public static final int MAX_MOV_LEVEL0 = 1;
-    public static final int MAX_MOV_LEVEL1 = 2;
-    public static final int MAX_MOV_LEVEL2 = 3;
+    public static final int MAX_MOV = 2;
     
     public Movement() {
-        switch(Config.instance.getLevel()) {
-            case 0: 
-                movements = new Vector<>(MAX_MOV_LEVEL0);
-                break;
-            case 1:
-                movements = new Vector<>(MAX_MOV_LEVEL1);
-                break;
-            case 2: 
-                movements = new Vector<>(MAX_MOV_LEVEL2);
-                break;
-            default:
-                movements = new Vector<>(MAX_MOV_LEVEL0);
-                break;
-        }
-        
+        movements = new Vector<>(MAX_MOV);
         movements.add(Direction.RIGHT);
+
     }
     
     public boolean isFull() {
-        switch(Config.instance.getLevel()) {
-            case 0:
-                if (movements.size() == MAX_MOV_LEVEL0 ) {
-                    return true;
-                }
-                break;
-            case 1:
-                if (movements.size() == MAX_MOV_LEVEL1 ) {
-                    return true;
-                }
-                break;
-            case 2:
-                if (movements.size() == MAX_MOV_LEVEL2 ) {
-                    return true;
-                }
-                break;
-            default:
-                if (movements.size() == MAX_MOV_LEVEL0) {
-                    return true;
-                }
-                break;
-        }
+        if (movements.size() == MAX_MOV ) {
+            return true;
+        }      
+        
         return false;
     }
     
@@ -73,14 +40,47 @@ public class Movement {
     
     public void insertNewMov(Direction d) {
         if (isFull()) {
-            movements.remove(0);
-        } 
-        movements.add(d);
+            if (newMovIsValid(d)) {
+                movements.remove(0);
+                movements.add(d); 
+            }
+        } else {
+            movements.add(d); 
+        }
+              
     }
+    
     public boolean isLastMove() {
         if (movements.size() <= 1) {
             return true;
         }
         return false;
+    }
+    
+    public boolean newMovIsValid(Direction d){
+         switch (d) {
+             case LEFT:
+                 if (movements.get(MAX_MOV - 1).equals(Direction.RIGHT)) {
+                     return false;
+                 }
+                 break;
+             case RIGHT:
+                 if (movements.get(MAX_MOV - 1).equals(Direction.LEFT)) {
+                     return false;
+                 }
+                 break;
+             case UP:
+                 if (movements.get(MAX_MOV - 1).equals(Direction.DOWN)) {
+                     return false;
+                 }
+                 break;
+             case DOWN:
+                 if (movements.get(MAX_MOV - 1).equals(Direction.UP)) {
+                     return false;
+                 }
+                 break;
+                 
+         }
+         return true;
     }
 }
